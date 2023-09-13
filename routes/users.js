1cffd2a0af3f28acb966a8d702cf5f9f49f6e991
Ryan Staats <rstaats0927@gmail.com>
2023-09-13 13:51:56 -0500

    has bug that won't make a post request

--- routes/users.js
+++ routes/users.js
@@ -1,17 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const createUser = require('../controllers/createUser');
-// const User = require('../models/user');
-
-// router.get('/users', (req, res) => {
-//   User.find({})
-//     .then(data => {
-//       res.send(data);
-//     })
-//     .catch(() => {
-//       res.status(404).send({message: "users not found"});
-//     })
-// })
 
 router.get('/', (req, res) => {
   res.send("Received a GET request at /users");
@@ -22,16 +11,6 @@ router.get('/:id', (req, res) => {
 
   res.send(`recieved a GET request for user id: ${id}`);
 })
-
-// router.get('/:id', (req, res) => {
-//   User.findById(req.params.id)
-//     .then(user => {
-//       res.send({data: user});
-//     })
-//     .catch(() => {
-//       res.status(404).send({message: "user not found"});
-//     })
-// })
 
 router.post('/', (req, res) => {
   createUser(req, res);
