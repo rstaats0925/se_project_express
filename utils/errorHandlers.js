db328da154a2bb6c005af17affe833c8263698fe
Ryan Staats <rstaats0927@gmail.com>
2023-09-16 17:26:28 -0500

    fixed a bug in handleItemHttpError

--- utils/errorHandlers.js
+++ utils/errorHandlers.js
@@ -10,7 +10,7 @@ function handleUserHttpError (req, res, err) {
       res.status(BAD_REQUEST).send({message: "id is incorrect format", err});
       break;
     case "ValidationError":
-      res.status(BAD_REQUEST).send({message:"id is incorrect format", err});
+      res.status(BAD_REQUEST).send({message:"id is incorrect format or information is missing", err});
       break;
     default:
       res.status(INTERNAL_SERVER_ERROR).send({message: `An ${err.name} error has occurred on the server`, err});
@@ -19,13 +19,16 @@ function handleUserHttpError (req, res, err) {
 }
 
 function handleItemHttpError (req, res, err) {
-  console.error(err);
+  console.error(err.name);
   switch(err.name) {
     case "DocumentNotFoundError":
       res.status(NOT_FOUND).send({message: `item id ${req.params.itemId} couldn't be found`, err});
       break;
     case "CastError":
       res.status(BAD_REQUEST).send({message: "id is incorrect format", err});
+      break;
+    case "ValidationError":
+      res.status(BAD_REQUEST).send({message: "id is incorrect format, or information is missing", err});
       break;
     default:
       res.status(INTERNAL_SERVER_ERROR).send({message: "An error has occurred on the server", err});
