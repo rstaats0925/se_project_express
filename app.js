0bcc1e7932af4af235f5a562cdb0378c8c0c8140
Ryan Staats <rstaats0927@gmail.com>
2023-09-15 21:24:48 -0500

    fixed syntax error in mongoose.connect()

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ const mongoose = require('mongoose');
 const app = express();
 
 mongoose.connect('mongodb://127.0.0.1:27017/wtwr_db', (r) => {
-  console.log("connected to db", r), e => console.log("db error", e)
-});
+  console.log("connected to db", r)}, e => console.log("db error", e)
+);
 
 const routes = require('./routes/index');
 
