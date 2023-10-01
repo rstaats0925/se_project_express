ba4be8e5af2337beba45693a6b35ec58d18f6172
Ryan Staats <rstaats0927@gmail.com>
2023-10-01 14:45:09 -0500

    project ready for testing

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require("cors");
 
 const { PORT = 3001 } = process.env;
 const mongoose = require('mongoose');
@@ -14,6 +15,7 @@ const routes = require('./routes/index');
 app.use(express.json());
 
 app.use(routes);
+app.use(cors());
 
 app.listen(PORT, () => {
   console.log(`App listening at port ${PORT}`);
